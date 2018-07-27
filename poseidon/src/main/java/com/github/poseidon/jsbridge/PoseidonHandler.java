14bcae9ff3bbe4d5047d868126ceb96808734234
Charles <1187340067@qq.com>
2018-07-27 10:41:49 +0800

    给poseidonHandler添加注释

--- poseidon/src/main/java/com/github/poseidon/jsbridge/PoseidonHandler.java
+++ poseidon/src/main/java/com/github/poseidon/jsbridge/PoseidonHandler.java
@@ -28,11 +28,14 @@ public abstract class PoseidonHandler {
     }
 
     /**
-     *
-     * @param action
-     * @param rawArgs
-     * @param callback
-     * @return 返回true，该handler才能正常运行，返回false，则js端将会收到"invalid action"的信息,默认情况下返回false.
+     * @param action   <div class="zh"></div>   要执行的action
+     *                 <div class = "en"></div>
+     * @param rawArgs  <div class="zh"></div>  JS端传递过来的JSON格式的参数
+     *                 <div class = "en"></div>
+     * @param callback <div class="zh"></div>  通过callback回调给JavaScript.
+     *                 <div class = "en"></div>
+     * @return <div class="zh"></div>  返回true，该handler才能正常运行，返回false，则js端将会收到"invalid action"的信息,默认情况下返回false.
+     * <div class = "en"></div>
      * @throws JSONException
      */
     public boolean execute(String action, String rawArgs, CallBack callback) throws JSONException {
@@ -40,18 +43,56 @@ public abstract class PoseidonHandler {
         return execute(action, args, callback);
     }
 
+    /**
+     * @param action   <div class="zh"></div>  要执行的action
+     *                 <div class = "en"></div>
+     * @param args     <div class="zh"></div>     JS端传递过来的JSON格式的参数
+     *                 <div class = "en"></div>
+     * @param callback <div class="zh"></div>  通过callback回调给JavaScript.
+     *                 <div class = "en"></div>
+     * @return <div class="zh"></div>  返回true，该handler才能正常运行，返回false，则js端将会收到"invalid action"的信息,默认情况下返回false.
+     * <div class = "en"></div>
+     * @throws JSONException
+     */
     public boolean execute(String action, JSONArray args, CallBack callback) throws JSONException {
         return false;
     }
 
+    /**
+     * <div class="zh">将指定的数据发送给JavaScript.</div>
+     * <div class = "en">Sends the specified data to JavaScript.</div>
+     *
+     * @param data <div class="zh">要发送的数据</div>
+     *             <div class = "en">The data to be sent</div>
+     */
     protected void dispatchedJSEvent(String data) {
         this.dispatchedJSEvent(null, data);
     }
 
+    /**
+     * <div class="zh">将指定的数据发送给JavaScript中注册的handler</div>
+     * <div class = "en">Sends the specified data to the handler registered in JavaScript</div>
+     *
+     * @param handlerName <div class="zh">JavaScript中注册handler的名字</div>
+     *                    <div class = "en">The name of the handler is registered in JavaScript</div>
+     * @param data        <div class="zh">要发送的数据</div>
+     *                    <div class = "en">The data to be sent</div>
+     */
     protected void dispatchedJSEvent(String handlerName, String data) {
         this.dispatchedJSEvent(handlerName, data, null);
     }
 
+    /**
+     * <div class="zh">将指定的数据发送给JavaScript中注册的handler，同时接收来自JavaScript的回执消息</div>
+     * <div class = "en">Sends the specified data to the handler registered in JavaScript and receives a receipt message from JavaScript</div>
+     *
+     * @param handlerName      <div class="zh">JavaScript中注册handler的名字</div>
+     *                         <div class = "en">The name of the handler is registered in JavaScript</div>
+     * @param data             <div class="zh">要发送的数据</div>
+     *                         <div class = "en">The data to be sent</div>
+     * @param responseCallback <div class="zh">用于接收JavaScript的回执消息的回调</div>
+     *                         <div class = "en">A callback used to receive a return message from JavaScript</div>
+     */
     protected void dispatchedJSEvent(String handlerName, String data, ResponseCallback responseCallback) {
         webview.callJshandler(handlerName, data, responseCallback);
     }
